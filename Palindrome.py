c136ad6b4b24d7773cec081a8174d1989f598e91
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-09-20 22:09:29 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the number: "))
+val = str(num)
+if val==val[::-1]:
+    print("The number is palindrome ")
+else:
+    print("This is not a palindrome number ")
+for i in range(10):
+    if val.count(str(i))>0:
+        print(str(i),"appears",val.count(str(i)),"Times")

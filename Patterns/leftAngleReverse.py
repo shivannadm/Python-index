e7c6065a4734f8e53465ac559af11583f65bf882
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-12-11 21:38:41 +0530

    Update leftAngleReverse.py

--- Patterns/leftAngleReverse.py
+++ Patterns/leftAngleReverse.py
@@ -4,3 +4,15 @@ for i in range(6):
     for j in range(i,6):
         print("*",end=' ')
     print("")
+
+'''
+    Output
+    
+    * * * * * * 
+      * * * * *
+        * * * *
+          * * *
+            * *
+              *  
+
+'''

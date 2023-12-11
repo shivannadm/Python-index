d59f3e37f5d7ace9482adfe2ad6cba9afd434139
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-12-11 21:35:52 +0530

    Update forLoop.py

--- Patterns/forLoop.py
+++ Patterns/forLoop.py
@@ -2,3 +2,17 @@ for i in range(2):
     for j in range(2):
         for k in range(2):
             print(i,j,k)
+
+'''
+    Output
+    
+    0 0 0
+    0 0 1
+    0 1 0
+    0 1 1
+    1 0 0
+    1 0 1
+    1 1 0
+    1 1 1   
+
+'''

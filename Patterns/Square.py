896e62c49672d36687a05bd7064d9601cf0d4f8d
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-12-11 21:32:34 +0530

    Update Square.py

--- Patterns/Square.py
+++ Patterns/Square.py
@@ -2,3 +2,16 @@ for i in range(6):
     for j in range(6):
         print("*",end=' ')
     print("")
+
+'''
+
+    Output
+
+* * * * * * 
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+
+'''

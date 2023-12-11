0b8b9dfe5eb14dd9115caa4bbb370587e1a105bf
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-12-11 21:33:25 +0530

    Update Triangle.py

--- Patterns/Triangle.py
+++ Patterns/Triangle.py
@@ -6,3 +6,15 @@ for i in range(6):
     for j in range(i):
         print("*",end=' ')
     print("")
+
+'''
+        Output
+        
+            * 
+          * * *
+        * * * * *        
+      * * * * * * *      
+    * * * * * * * * *    
+  * * * * * * * * * * *
+
+'''

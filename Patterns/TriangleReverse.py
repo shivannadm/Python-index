c82e67a04ca68d414a5b329a9758edc5f78f799b
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-12-11 21:34:39 +0530

    Update TriangleReverse.py

--- Patterns/TriangleReverse.py
+++ Patterns/TriangleReverse.py
@@ -6,3 +6,14 @@ for i in range(6):
     for j in range(i,6):
         print("*",end=' ')
     print("")
+
+'''
+        Output
+
+  * * * * * * * * * * * 
+    * * * * * * * * *    
+      * * * * * * *      
+        * * * * *        
+          * * *
+            *
+'''

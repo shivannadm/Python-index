f6aeb78fcb8ebfe39928180ee806dff40c2688dd
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2023-12-12 22:14:08 +0530

    Update rightAngle.py

--- Patterns/rightAngle.py
+++ Patterns/rightAngle.py
@@ -2,3 +2,15 @@ for i in range(6):
     for j in range(i+1):
         print("*",end=' ')
     print("")
+
+'''
+    Output
+
+    * 
+    * * 
+    * * * 
+    * * * * 
+    * * * * * 
+    * * * * * * 
+
+'''

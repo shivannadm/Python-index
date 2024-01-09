f6ca499998fec481ab55dbca5a068311805b787c
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2024-01-09 20:59:33 +0530

    Update NumTriangleL.py

--- Patterns/NumTriangleL.py
+++ Patterns/NumTriangleL.py
@@ -42,24 +42,27 @@ print("\n")
     '''
 
 
-''' Numbers with increasing order
+''' 
 
-# p = 1
-# for i in range(6):
-#     for j in range(i,6):
-#         print(" ",end=' ')
-#     for j in range(i+1):
-#         print(p,end=' ')
-#         p += 1
-#     print()
+Numbers with increasing order
+
+ p = 1
+ for i in range(6):
+     for j in range(i,6):
+         print(" ",end=' ')
+     for j in range(i+1):
+         print(p,end=' ')
+         p += 1
+     print()
 
 
-#     '''
-#      Output   
-#             1
-#           2 3
-#         4 5 6
-#       7 8 9 10
-#     11 12 13 14 15      
-#   16 17 18 19 20 21
-#     '''
+     
+      Output   
+             1
+           2 3
+         4 5 6
+       7 8 9 10
+     11 12 13 14 15      
+   16 17 18 19 20 21
+   
+    '''
